524a8f23d55ab82c09b83b72b0cf86cad939862d
Sajjad Islam <s.i.sajjad.islam@gmail.com>
2022-05-25 02:29:04 -0500

    Add 3-label and 4-label assertion model

--- Assertion/modify_label.py
+++ Assertion/modify_label.py
@@ -1,6 +1,9 @@
 from util import config
 import pandas as pd
 
+'''
+Modify train, test data  original label to specific label
+'''
 
 def replace_ast_label(ast_label):
     if ast_label == 'present':
@@ -17,6 +20,17 @@ def replace_ast_3_label(ast_label):
         return 'present'
     elif ast_label == 'possible':
         return 'possible'
+    else:
+        return 'not-present'
+    return ''
+
+def replace_ast_4_label(ast_label):
+    if ast_label == 'present':
+        return 'present'
+    elif ast_label == 'possible':
+        return 'possible'
+    elif ast_label == 'conditional':
+        return 'conditional'
     else:
         return 'not-present'
     return ''
@@ -55,6 +69,7 @@ def main():
     train_df.to_csv(config.OUT_FILES['assertion_label_modified_train'], sep="\t")
     
     '''
+    '''
     print('=================================')
 
     dev_df['label'] = dev_df['label'].apply(lambda ast_label: replace_ast_3_label(ast_label))
@@ -73,6 +88,27 @@ def main():
     # dev_df.to_csv(config.OUT_FILES['assertion_3_label_modified_dev'], sep="\t")
     # test_df.to_csv(config.OUT_FILES['assertion_3_label_modified_test'], sep="\t")
     # train_df.to_csv(config.OUT_FILES['assertion_3_label_modified_train'], sep="\t")
+    '''
+    
+    print('=================================')
+
+    dev_df['label'] = dev_df['label'].apply(lambda ast_label: replace_ast_4_label(ast_label))
+    test_df['label'] = test_df['label'].apply(lambda ast_label: replace_ast_4_label(ast_label))
+    train_df['label'] = train_df['label'].apply(lambda ast_label: replace_ast_4_label(ast_label))
+
+    print('Train data (modified label counts)') 
+    print(train_df['label'].value_counts())
+    print('---------------------------------')
+    print('Dev data (modified label counts)')
+    print(dev_df['label'].value_counts())
+    print('---------------------------------')
+    print('Test data (modified label counts)')
+    print(test_df['label'].value_counts())
+
+    #dev_df.to_csv(config.OUT_FILES['assertion_4_label_modified_dev'], sep="\t")
+    #test_df.to_csv(config.OUT_FILES['assertion_4_label_modified_test'], sep="\t")
+    #train_df.to_csv(config.OUT_FILES['assertion_4_label_modified_train'], sep="\t")
+
 
 
 if __name__ == "__main__":
